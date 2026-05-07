9eafe202ff87e45ea03ce76c583c830218daa788
VictorLopesD <46869529+VictorLopesD@users.noreply.github.com>
2026-05-07 14:03:09 -0300

    primeiro commit

---
+++ GradeLaboratoriosFECAP/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace GradeLaboratoriosFECAP
+{
+    partial class PanelSup
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            splitContainer1 = new SplitContainer();
+            radioButton5 = new RadioButton();
+            radioButton3 = new RadioButton();
+            radioButton4 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            listView1 = new ListView();
+            tabControl2 = new TabControl();
+            tabPage3 = new TabPage();
+            tabPage4 = new TabPage();
+            tabPage5 = new TabPage();
+            SegHorarioNot = new GroupBox();
+            PrimHorarioNot = new GroupBox();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            Lab106B = new GroupBox();
+            InfoLab106 = new Label();
+            Lab200B = new GroupBox();
+            tabPage2 = new TabPage();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            tabControl2.SuspendLayout();
+            tabPage5.SuspendLayout();
+            PrimHorarioNot.SuspendLayout();
+            flowLayoutPanel1.SuspendLayout();
+            Lab106B.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(800, 450);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(splitContainer1);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(792, 422);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "GRADE FIXA";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(3, 3);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(radioButton5);
+            splitContainer1.Panel1.Controls.Add(radioButton3);
+            splitContainer1.Panel1.Controls.Add(radioButton4);
+            splitContainer1.Panel1.Controls.Add(radioButton2);
+            splitContainer1.Panel1.Controls.Add(radioButton1);
+            splitContainer1.Panel1.Controls.Add(listView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(tabControl2);
+            splitContainer1.Size = new Size(786, 416);
+            splitContainer1.SplitterDistance = 113;
+            splitContainer1.TabIndex = 0;
+            // 
+            // radioButton5
+            // 
+            radioButton5.AutoSize = true;
+            radioButton5.Location = new Point(5, 95);
+            radioButton5.Name = "radioButton5";
+            radioButton5.Size = new Size(92, 19);
+            radioButton5.TabIndex = 6;
+            radioButton5.TabStop = true;
+            radioButton5.Text = "SEXTA-FEIRA";
+            radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(5, 70);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(102, 19);
+            radioButton3.TabIndex = 5;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "QUINTA-FEIRA";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(5, 49);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(104, 19);
+            radioButton4.TabIndex = 4;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "QUARTA-FEIRA";
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(5, 24);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(95, 19);
+            radioButton2.TabIndex = 3;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "TERÇA-FEIRA";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(5, 3);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(113, 19);
+            radioButton1.TabIndex = 2;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "SEGUNDA-FEIRA";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // listView1
+            // 
+            listView1.Dock = DockStyle.Fill;
+            listView1.Location = new Point(0, 0);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(113, 416);
+            listView1.TabIndex = 1;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.SmallIcon;
+            // 
+            // tabControl2
+            // 
+            tabControl2.Controls.Add(tabPage3);
+            tabControl2.Controls.Add(tabPage4);
+            tabControl2.Controls.Add(tabPage5);
+            tabControl2.Dock = DockStyle.Fill;
+            tabControl2.Location = new Point(0, 0);
+            tabControl2.Name = "tabControl2";
+            tabControl2.SelectedIndex = 0;
+            tabControl2.Size = new Size(669, 416);
+            tabControl2.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            tabPage3.Location = new Point(4, 24);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Padding = new Padding(3);
+            tabPage3.Size = new Size(661, 388);
+            tabPage3.TabIndex = 0;
+            tabPage3.Text = "Matutino";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            tabPage4.Location = new Point(4, 24);
+            tabPage4.Name = "tabPage4";
+            tabPage4.Padding = new Padding(3);
+            tabPage4.Size = new Size(661, 388);
+            tabPage4.TabIndex = 1;
+            tabPage4.Text = "Vespertino";
+            tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            tabPage5.Controls.Add(SegHorarioNot);
+            tabPage5.Controls.Add(PrimHorarioNot);
+            tabPage5.Location = new Point(4, 24);
+            tabPage5.Name = "tabPage5";
+            tabPage5.Padding = new Padding(3);
+            tabPage5.Size = new Size(661, 388);
+            tabPage5.TabIndex = 2;
+            tabPage5.Text = "Noturno";
+            tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // SegHorarioNot
+            // 
+            SegHorarioNot.Location = new Point(6, 189);
+            SegHorarioNot.Name = "SegHorarioNot";
+            SegHorarioNot.Size = new Size(649, 180);
+            SegHorarioNot.TabIndex = 1;
+            SegHorarioNot.TabStop = false;
+            SegHorarioNot.Text = "21h00 às 22h40";
+            // 
+            // PrimHorarioNot
+            // 
+            PrimHorarioNot.Controls.Add(flowLayoutPanel1);
+            PrimHorarioNot.Location = new Point(6, 3);
+            PrimHorarioNot.Name = "PrimHorarioNot";
+            PrimHorarioNot.Size = new Size(649, 180);
+            PrimHorarioNot.TabIndex = 0;
+            PrimHorarioNot.TabStop = false;
+            PrimHorarioNot.Text = "19h00 às 20h40";
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(Lab106B);
+            flowLayoutPanel1.Controls.Add(Lab200B);
+            flowLayoutPanel1.Dock = DockStyle.Fill;
+            flowLayoutPanel1.Location = new Point(3, 19);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(643, 158);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // Lab106B
+            // 
+            Lab106B.Controls.Add(InfoLab106);
+            Lab106B.Location = new Point(3, 3);
+            Lab106B.Name = "Lab106B";
+            Lab106B.Size = new Size(137, 67);
+            Lab106B.TabIndex = 0;
+            Lab106B.TabStop = false;
+            Lab106B.Text = "106B";
+            // 
+            // InfoLab106
+            // 
+            InfoLab106.AllowDrop = true;
+            InfoLab106.Dock = DockStyle.Fill;
+            InfoLab106.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 2);
+            InfoLab106.Location = new Point(3, 19);
+            InfoLab106.Name = "InfoLab106";
+            InfoLab106.RightToLeft = RightToLeft.Yes;
+            InfoLab106.Size = new Size(131, 45);
+            InfoLab106.TabIndex = 0;
+            InfoLab106.Text = "4NADS - ENG. SOFTWARE  E ARQ. SIST. - LUCY";
+            InfoLab106.TextAlign = ContentAlignment.TopCenter;
+            InfoLab106.UseCompatibleTextRendering = true;
+            // 
+            // Lab200B
+            // 
+            Lab200B.Location = new Point(146, 3);
+            Lab200B.Name = "Lab200B";
+            Lab200B.Size = new Size(137, 67);
+            Lab200B.TabIndex = 1;
+            Lab200B.TabStop = false;
+            Lab200B.Text = "200B";
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(792, 422);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // PanelSup
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tabControl1);
+            Name = "PanelSup";
+            Text = "  ";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            tabControl2.ResumeLayout(false);
+            tabPage5.ResumeLayout(false);
+            PrimHorarioNot.ResumeLayout(false);
+            flowLayoutPanel1.ResumeLayout(false);
+            Lab106B.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private SplitContainer splitContainer1;
+        private TabPage tabPage2;
+        private TabControl tabControl2;
+        private TabPage tabPage3;
+        private TabPage tabPage4;
+        private TabPage tabPage5;
+        private GroupBox SegHorarioNot;
+        private GroupBox PrimHorarioNot;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private GroupBox Lab106B;
+        private Label InfoLab106;
+        private GroupBox Lab200B;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private ListView listView1;
+        private RadioButton radioButton3;
+        private RadioButton radioButton4;
+        private RadioButton radioButton5;
+    }
+}
---
+++ GradeLaboratoriosFECAP/Form1.cs
@@ -0,0 +1,15 @@
+namespace GradeLaboratoriosFECAP
+{
+    public partial class PanelSup : Form
+    {
+        public PanelSup()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
